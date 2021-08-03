785a9bccdf9c8f556ce01571896e141284adc68a
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-03 17:39:55 -0300

    created hook

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div></div>
+  <h1>{{ message }}</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      message: 'Hello world'
+    }
+  },
+  beforeCreate() {
+    console.log('Antes da criação')
+  },
+  created() {
+    console.log('Depois da criação')
+  }
+})
 </script>
 
 <style scoped></style>
