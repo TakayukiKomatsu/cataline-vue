b705aa852858421bbf20f0956fc6c5b524ac0809
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-03 18:25:04 -0300

    v-html

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <a :href="product.url" :class="productClass">{{ product.name }}</a>
-
-  OR
-
-  <a v-bind:href="product.url" v-bind:class="productClass">{{
-    product.name
-  }}</a>
+  <p>Acesse: <span v-html="link" /></p>
 </template>
 
 <script lang="ts">
@@ -14,26 +8,10 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      product: {
-        name: 'Camisa',
-        url: 'http://loja.com/produtos/12345',
-        stock: true
-      }
-    }
-  },
-  computed: {
-    productClass(): string {
-      return this.product.stock ? 'success' : 'danger'
+      link: '<a href="https://cataline.io">Cataline</a>'
     }
   }
 })
 </script>
 
-<style scoped>
-.danger {
-  color: red;
-}
-.success {
-  color: green;
-}
-</style>
+<style scoped></style>
