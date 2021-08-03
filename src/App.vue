da6af165bed916e85782978fea73f0e3e94b707d
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-03 17:31:18 -0300

    simple exercise about vue properties

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,52 @@
 <template>
-  <h1>{{ person }}</h1>
+  <p v-for="todo in doneTodos" :key="todo.text">
+    {{ todo.text }}
+  </p>
+
+  <button @click="checkAllTodos">Finalizar</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+interface Todo {
+  text: string
+  done: boolean
+}
+
 export default defineComponent({
   data() {
     return {
-      person: {} as Person
+      todos: [] as Todo[]
+    }
+  },
+  computed: {
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.done)
+    }
+  },
+  watch: {
+    todos(newValue: Todo[]) {
+      const isFinished = !newValue.some(({ done }) => {
+        !done
+      })
+      if (isFinished) {
+        alert('Yuuup')
+      }
+    }
+  },
+  created() {
+    this.todos = [
+      { text: 'Estudar Typescript', done: true },
+      { text: 'Lavar os pratos', done: false },
+      { text: 'Aprender Nuxt.js', done: true }
+    ]
+  },
+  methods: {
+    checkAllTodos() {
+      this.todos = this.todos.map(({ text }) => {
+        return { text, done: true }
+      })
     }
   }
 })
