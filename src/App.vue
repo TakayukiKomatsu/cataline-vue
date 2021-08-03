61eda3e942b4901a66056194e7ac98d6b79c6b07
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-03 18:09:00 -0300

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>Hello world</h1>
+  <a :href="product.url" :class="productClass">{{ product.name }}</a>
+
+  OR
+
+  <a v-bind:href="product.url" v-bind:class="productClass">{{
+    product.name
+  }}</a>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      product: {
+        name: 'Camisa',
+        url: 'http://loja.com/produtos/12345',
+        stock: true
+      }
+    }
+  },
+  computed: {
+    productClass(): string {
+      return this.product.stock ? 'success' : 'danger'
+    }
+  }
+})
 </script>
+
+<style scoped>
+.danger {
+  color: red;
+}
+.success {
+  color: green;
+}
+</style>
